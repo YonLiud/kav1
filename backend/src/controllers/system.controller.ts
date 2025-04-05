921dd9644a9572e17a6842ef87b691391562e62e
Yon Liud <ejliud@gmail.com>
2025-04-05 10:56:28 +0300

    refactor: update ping function to return an object instead of sending a WebSocket message

--- backend/src/controllers/system.controller.ts
+++ backend/src/controllers/system.controller.ts
@@ -1,5 +1,3 @@
-import { WebSocket } from 'ws';
-
-export const ping = (ws: WebSocket) => {
-    ws.send(JSON.stringify({ type: 'pong' }));
-}; 
+export const ping = (): any => {
+  return { type: 'pong' };
+};
