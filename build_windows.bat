b2f9ebd523bac43f35635469aafb0df68a6ef641
Yon Liud <ejliud@gmail.com>
2025-05-29 18:10:06 +0300

    feat: implement version tracking from git for client

--- build_windows.bat
+++ build_windows.bat
@@ -1,6 +1,16 @@
+@echo off
 cls
+
+for /f %%i in ('git rev-parse --short HEAD') do set GIT_HASH=%%i
+echo %GIT_HASH% > frontend\version.txt
+echo %GIT_HASH% > backend\version.txt
+
+set RELEASE_BUILD=false
+
 cd frontend
 pyinstaller --noconfirm main.spec
+cd ..
 
-cd ..\backend
+cd backend
 pyinstaller --noconfirm server.spec
+cd ..
